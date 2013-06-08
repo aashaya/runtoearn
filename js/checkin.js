295d4c0f34d11208a48f139b04701cc50b2f0576
aashaya <aaschyist@gmail.com>
2013-06-08 12:40:12 -0300

    Update checkin.js

--- js/checkin.js
+++ js/checkin.js
@@ -6,29 +6,19 @@ Created      : 06/07/2013
 
 $(function () {
    $('.checkin').click(function () {
-      if(navigator.geolocation){
-         // timeout at 60000 milliseconds (60 seconds)
-         var options = {timeout:60000};
-         navigator.geolocation.getCurrentPosition(showLocation, 
-                                                  errorHandler,
-                                                  options);
-      }else{
-         alert("Sorry, browser does not support geolocation!");
-      }
-      
-    }); 
+      if(navigator.geolocation) {
+   			navigator.geolocation.getCurrentPosition(function(position){
+					initialize(position.coords.latitude,position.coords.longitude);
+				});
+			}
 });
 
-function showLocation(position) {
-  var latitude = position.coords.latitude;
-  var longitude = position.coords.longitude;
-  alert("Latitude : " + latitude + " Longitude: " + longitude);
-}
-
-function errorHandler(err) {
-  if(err.code == 1) {
-    alert("Error: Access is denied!");
-  }else if( err.code == 2) {
-    alert("Error: Position is unavailable!");
-  }
-}
+function initialize(lat,lng) {
+   		var latlng = new google.maps.LatLng(lat, lng);
+			var myOptions = {
+				zoom: 8,
+				center: latlng,
+				mapTypeId: google.maps.MapTypeId.ROADMAP
+		    };
+		    var map = new google.maps.Map(document.getElementById("map_canvas"),myOptions);
+		}
