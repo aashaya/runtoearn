e7d80733ff08fba8a4dbe8241ade327af316d563
aashaya <aaschyist@gmail.com>
2013-06-08 13:52:54 -0300

    Update checkin.js

--- js/checkin.js
+++ js/checkin.js
@@ -1,24 +1,13 @@
-﻿/*
+/*
 Name         : checkin.js
 Author       : Aashaya Bhattarai
 Created      : 06/07/2013
 */
 
 $(function () {
-   $('.checkin').click(function () {
-      if(navigator.geolocation) {
-   			navigator.geolocation.getCurrentPosition(function(position){
-					initialize(position.coords.latitude,position.coords.longitude);
-				});
-			}
+    $("#btnCheckin").click(function () {
+        var lat = $("#lat").val();
+        var lng = $("#lng").val();
+        alert("lat: " + lat + " lng: " + lng);
+    });
 });
-
-function initialize(lat,lng) {
-   		var latlng = new google.maps.LatLng(lat, lng);
-			var myOptions = {
-				zoom: 8,
-				center: latlng,
-				mapTypeId: google.maps.MapTypeId.ROADMAP
-		    };
-		    var map = new google.maps.Map(document.getElementById("map_canvas"),myOptions);
-		}
