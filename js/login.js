88334a214c3c32346c5e1d33c815c2b840cdef07
aashaya <aaschyist@gmail.com>
2013-06-10 14:03:28 -0400

    maps added.

--- js/login.js
+++ js/login.js
@@ -6,8 +6,9 @@ Created      : 06/07/2013
 
 $(function () {
     $('.frmlogin').click(function () {
-        if ($("#Username").val() == 'test' && $("#Password").val() == 'abc123') {
+        /*if ($("#Username").val() == 'test' && $("#Password").val() == 'abc123') {
             $.mobile.changePage("checkin.html", { transition: "none" });
-        }
+        }*/
+		window.location.href = "checkin.html";
     });
 });
