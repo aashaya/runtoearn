d91275a04b842c17d361a33307b5ff5431a6dd6d
aashaya <aaschyist@gmail.com>
2013-06-11 11:08:51 -0300

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,4 +1,4 @@
-﻿﻿/*
+﻿/*
 Name         : login.js
 Author       : Aashaya Bhattarai
 Created      : 06/07/2013
@@ -9,10 +9,6 @@ $(function () {
         /*if ($("#Username").val() == 'test' && $("#Password").val() == 'abc123') {
             $.mobile.changePage("checkin.html", { transition: "none" });
         }*/
-<<<<<<< HEAD
 		window.location.href = "checkin.html";
-=======
-    	window.location.href = "checkin.html";
->>>>>>> 2daf73087d8f13f32403f4ec906488e5ff6b0999
     });
 });
