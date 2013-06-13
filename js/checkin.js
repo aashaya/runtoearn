9f15d3ec3722f0f5838845640f1ee73cf85bd6df
aashaya <aaschyist@gmail.com>
2013-06-12 23:14:14 -0300

    Update checkin.js

--- js/checkin.js
+++ js/checkin.js
@@ -6,8 +6,6 @@ Created      : 06/07/2013
 
 $(function () {
     $("#btnCheckin").click(function () {
-        var lat = $("#lat").val();
-        var lng = $("#lng").val();
-        alert("lat: " + lat + " lng: " + lng);
+        alert("under construction!");
     });
 });
